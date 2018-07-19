2b918fd8dc20b62fac886109afcf93ed0367c966
AREUM AN <areum.an.0928@gmail.com>
2018-07-18 22:34:26 -0400

    __REDUX_DEVTOOLS_EXTENSION_

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer);
+//const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
